d377881e98666242702c014e9d9df5ad7559238e
hasmorgado28 <has.morgado@gmail.com>
2026-09-18 01:34:46 -0600

    variables de entorno

---
+++ src/routes/indexRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { createProductos, deleteProductos, renderEditProducto, renderProductos, statusProductos, updateProductos } from "../controllers/productoController";
+import Productos from "../models/Productos";
+
+
+const router = Router();
+
+router.get("/", renderProductos);
+
+router.post("/productos/agregar", createProductos);
+
+router.get("/productos/:id/update", renderEditProducto);
+
+router.post("/productos/:id/update", updateProductos);
+
+router.get("/productos/:id/delete", deleteProductos);
+
+router.get("/productos/:id/status", statusProductos);
+
+export default router;
